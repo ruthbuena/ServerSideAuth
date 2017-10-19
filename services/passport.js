73daf04a96da26b0dc40d9947bd63e3fc59defde
Ruth Buenaflor <ruthbpeace@gmail.com>
2017-10-18 21:02:06 -0400

    create passport.js service to authenticate users

---
+++ services/passport.js
@@ -0,0 +1,6 @@
+//Passport used to authenticate users
+const passport = require('passport');
+const User = require('../models/user');
+const config = require('../config');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;

b3c2e51e7e3e440546d31aae5babdef1b889aede
Ruth Buenaflor <ruthbpeace@gmail.com>
2017-10-18 20:39:07 -0400

    update schema with bcrypt hash

--- models/user.js
+++ models/user.js
@@ -10,15 +10,22 @@ const userSchema = new Schema({
 });
 
 //On Save Hook, encrypt password
+//Before saving a model, run this function (that is what .pre is)
 userSchema.pre('save', function(next) {
+
+  //get access to user model; instance of user model
   const user = this;
 
+  //use bcrypt library; generate a salt, take time then run callback
+  //after salt is created
   bcrypt.genSalt(10,function(err, salt) {
     if(err) { return next(err); }
 
+    //hast (aka encrypt) our password using salt
     bcrypt.hash(user.password, salt, null, function(err, hash) {
       if(err) { return next(err); }
 
+      //overwrite plain text password with encrypted password
       user.password = hash;
       next();
     });

54af85a661d01132d5030bc93af56fc65c5ace00
Ruth Buenaflor <ruthbpeace@gmail.com>
2017-10-18 21:14:16 -0400

    update jWT options to auth user

--- services/passport.js
+++ services/passport.js
@@ -4,3 +4,29 @@ const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+//Set up options for JWT Strategy
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromHeader('authorization')
+  secretOrKey: config.secret
+};
+
+//Create JWT Strategy
+//payload is the decoded JWT token
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
+  //See if the user ID in the payload exists in our database
+  //If it does, call 'done' with that other
+  //Otherwise, call done without a user object
+  User.findById(payload.sub, function() {
+    if(err) { return done(err, false); }
+
+    if(user) {
+      done(null, user);
+    } else {
+      done(null, false);
+    }
+  });
+});
+
+//Tell passport to use this Strategy
+passport.use(jwtLogin);
